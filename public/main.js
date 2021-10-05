1e103609e6f70ce2e4386dd3eba5d48a6402676a
Denny Min <denden223@gmail.com>
2021-10-04 20:38:05 -0700

    Enabled cors on my server so I can make fetch requests to my own api now. - Denny Min

--- public/main.js
+++ public/main.js
@@ -1,1 +1,10 @@
 const $Spotify = document.querySelector('.spotify');
+const $test = document.querySelector('.test');
+
+$test.addEventListener('click', () => {
+  fetch('http://localhost:3000/test', {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+})
--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.listen(3000, () => {
   console.log(`express server listening on port 3000`)
 })
 
-app.get('', (req, res, next) => {
+app.get('/test', (req, res, next) => {
   res.status(200).json('hi')
 }
 )
