69cfc770fa40f42a7f4a32c388ce13993b4f1ecf
Denny Min <denden223@gmail.com>
2021-10-04 20:24:28 -0700

    Originally planned for this to be an application that was only in the front end. However, just set up the server so that I can make oauth api calls. - Denny Min

--- server/app.js
+++ server/app.js
@@ -1,1 +1,12 @@
 require('dotenv').config()
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log(`express server listening on port 3000`)
+})
+
+app.get('', (req, res, next) => {
+  res.status(200).json('hi')
+}
+)
